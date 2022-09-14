c12f3bc1dd7a23e6a4526c1fbd27d257f6a9c7a1
victorbillaud <victorbillaud.fr>
2022-09-14 09:25:21 +0200

    add Dockerfile and docker-compose for launch project

--- back-express/db.js
+++ back-express/db.js
@@ -1,16 +1,19 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
+
+
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
+  host: "mysql_server",
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWD,
   database: process.env.SQL_DATABASE,
+  multipleStatements: true
 });
 
 connection.connect(function(err) {
+    if (err) throw err;    
     console.log("connected to database")
-    if (err) throw err;
 });
 
 module.exports = connection;
--- back-express/index.js
+++ back-express/index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
+const fs = require('fs')
+
+var path = "GiskardCalendarExercise.sql" 
+
 //Import routes
 const {
     AVAILABILITIES,
@@ -17,10 +21,11 @@ const {
 //Import controllers
 const availabilitiesServices = require('./lib/availabilities.js');
 const reservationsServices = require('./lib/reservations.js');
+const db = require("./db")
 
 // Init app
 const app = express();
-app.use(express.urlencoded({ extended: false }));   
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(fileUpload())
 app.use(express.static(__dirname + '/public'));
@@ -35,4 +40,14 @@ app.post(CREATE_RESERVATION, reservationsServices.addOneReservations, availabili
 app.post(DELETE_RESERVATION, reservationsServices.deleteOneReservations);
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) throw err
+        db.query(
+            data.toString(),
+            function (err, results, fields) {
+                //console.log(results); // results contains rows returned by server
+                //console.log(fields); // fields contains extra meta data about results, if available
+            }
+        );
+    })
 })
